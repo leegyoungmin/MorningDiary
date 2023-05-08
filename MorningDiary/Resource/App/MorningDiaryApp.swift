b7bbc28584ddf0e9783e31156abd86178de36d49
미니 <cow970814@naver.com>
2023-05-08 23:22:26 +0900

    feat : Project Start

---
+++ MorningDiary/Resource/App/MorningDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorningDiaryApp.swift
+//  MorningDiary
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+        
+
+import SwiftUI
+
+@main
+struct MorningDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
