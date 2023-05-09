d75fb97badcf24e09f6f77d9b69fac9521b5756f
미니 <cow970814@naver.com>
2023-05-09 13:07:00 +0900

    feat: Diary Board View 타입 생성

---
+++ MorningDiary/Resource/Scene/DiaryBoardView/DiaryBoardView.swift
@@ -0,0 +1,21 @@
+//
+//  DiaryBoardView.swift
+//  MorningDiary
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import SwiftUI
+
+struct DiaryBoardView: View {
+  var body: some View {
+    VStack {
+      Text("Example")
+    }
+  }
+}
+
+struct DiaryBoardView_Previews: PreviewProvider {
+  static var previews: some View {
+    DiaryBoardView()
+  }
+}
