77b7b14405c7b6c4be95e94f3c9c583ebc1a23ea
미니 <cow970814@naver.com>
2023-05-09 16:49:01 +0900

    refactor : Board View 하위 뷰들 변수로 분리

--- MorningDiary/Resource/Scene/DiaryBoardView/DiaryBoardView.swift
+++ MorningDiary/Resource/Scene/DiaryBoardView/DiaryBoardView.swift
@@ -7,9 +7,64 @@
 import SwiftUI
 
 struct DiaryBoardView: View {
+  @State private var mockDiaries = Diary.mockDiary
   var body: some View {
+    HStack(alignment: .center, spacing: .zero) {
+      
+      listSection
+        .frame(maxWidth: 450)
+      
+      detailDiarySection
+        .padding()
+    }
+    .ignoresSafeArea(.keyboard)
+  }
+}
+
+private extension DiaryBoardView {
+  var listSection: some View {
     VStack {
-      Text("Example")
+      HStack(alignment: .center) {
+        Spacer()
+        
+        Text("모닝 일기")
+        
+        Spacer()
+      }
+      
+      DiaryListView(diaries: mockDiaries)
+      
+    }
+  }
+  
+  var detailDiarySection: some View {
+    VStack(alignment: .leading) {
+      HStack(alignment: .center) {
+        Spacer()
+        
+        Button {
+          print("Tapped Save")
+        } label: {
+          Image(systemName: "checkmark")
+        }
+        .font(.system(size: 28, weight: .light))
+      }
+      
+      TextField("제목을 입력하세요.", text: .constant(""))
+        .font(.system(size: 34, weight: .black))
+      
+      TextEditor(text: .constant(""))
+      
+      HStack {
+        Button {
+          print("Tapped")
+        } label: {
+          Image(systemName: "photo.on.rectangle.angled")
+            .font(.title2)
+        }
+        
+        Spacer()
+      }
     }
   }
 }
