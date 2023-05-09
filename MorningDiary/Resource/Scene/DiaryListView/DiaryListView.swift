bff7552b1abc54e5f0f311dffe8ebe537ecbf0b7
미니 <cow970814@naver.com>
2023-05-09 16:48:28 +0900

    feat : Diary List View 타입 구현

--- MorningDiary/Resource/Scene/DiaryListView/DiaryListView.swift
+++ MorningDiary/Resource/Scene/DiaryListView/DiaryListView.swift
@@ -3,6 +3,44 @@
 //  MorningDiary
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
-import Foundation
+import SwiftUI
+
+struct DiaryListView: View {
+  let diaries: [Diary]
+  var body: some View {
+    ScrollView(.vertical, showsIndicators: false) {
+      ForEach(diaries, id: \.id) { diary in
+        VStack(alignment: .leading) {
+          Text(diary.createdDate.description)
+          
+          ForEach(diary.contents, id: \.id) { content in
+            HStack {
+              VStack(alignment: .leading) {
+                Text(content.title)
+                Text(content.body)
+              }
+              
+              Spacer()
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(12)
+          }
+        }
+        .padding()
+      }
+    }
+    .background(
+      Color.gray
+        .opacity(0.3)
+        .edgesIgnoringSafeArea(.all)
+    )
+  }
+}
+
+struct DiaryListView_Previews: PreviewProvider {
+  static var previews: some View {
+    DiaryListView(diaries: Diary.mockDiary)
+  }
+}
