bee0e17fb314b7d04529fdac39c4200405f8faf6
미니 <cow970814@naver.com>
2023-05-12 15:26:49 +0900

    feat : Diary 수정 중 다른 Diary 선택시 상태를 리셋시키는 기능 구현

--- MorningDiary/Resource/Scene/DetailDiaryView/DetailDiaryView.swift
+++ MorningDiary/Resource/Scene/DetailDiaryView/DetailDiaryView.swift
@@ -11,6 +11,13 @@ struct DetailDiaryView: View {
   @State private var editMode: Bool = false
   @State private var showMenu: Bool = false
   
+  func resetState(value: DiaryContent?) {
+    withAnimation {
+      showMenu = false
+      editMode = false
+    }
+  }
+  
   var body: some View {
     if let content = selectedContent {
       if editMode == false {
@@ -23,10 +30,12 @@ struct DetailDiaryView: View {
         .transition(.opacity.animation(.easeIn))
       } else {
         DiaryEditView(content: content)
+          .onChange(of: selectedContent, perform: resetState)
       }
     } else {
       // TODO: - EditMode View 구현 (기본으로 작성할 수 있는 공간)
       DiaryEditView()
+        .onChange(of: selectedContent, perform: resetState)
     }
   }
 }
