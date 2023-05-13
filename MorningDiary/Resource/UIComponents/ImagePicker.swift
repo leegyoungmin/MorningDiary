b9b639deadaab24707d3577debc29823e3d432ec
미니 <cow970814@naver.com>
2023-05-14 00:00:01 +0900

    feat : 이미지 선택 Controller 생성

---
+++ MorningDiary/Resource/UIComponents/ImagePicker.swift
@@ -0,0 +1,58 @@
+//
+//  ImagePicker.swift
+//  MorningDiary
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import SwiftUI
+import PhotosUI
+
+struct Photo: Identifiable {
+  let id = UUID()
+  let image: UIImage
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+  @Binding var selectedImages: [Photo]
+  
+  func makeUIViewController(context: Context) -> some UIViewController {
+    var configuration = PHPickerConfiguration()
+    configuration.selectionLimit = 5
+    configuration.filter = .images
+    
+    let controller = PHPickerViewController(configuration: configuration)
+    controller.isEditing = true
+    controller.delegate = context.coordinator
+    
+    return controller
+  }
+  
+  func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) { }
+  
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(parent: self)
+  }
+  
+  class Coordinator: NSObject, UINavigationControllerDelegate, PHPickerViewControllerDelegate {
+    let parent: ImagePicker
+    
+    init(parent: ImagePicker) {
+      self.parent = parent
+    }
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+      picker.dismiss(animated: true)
+      
+      results.forEach {
+        $0.itemProvider.loadObject(ofClass: UIImage.self) { object, error in
+          if let image = object as? UIImage {
+            let photo = Photo(image: image)
+            withAnimation {
+              self.parent.selectedImages.append(photo)
+            }
+          }
+        }
+      }
+    }
+  }
+}
