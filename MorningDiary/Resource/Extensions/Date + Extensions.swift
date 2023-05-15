324480ed91aacc644a413449f674f13c36f6a4cc
미니 <cow970814@naver.com>
2023-05-16 00:00:05 +0900

    feat : Date Extension에 OnlyDate 메서드 생성

--- MorningDiary/Resource/Extensions/Date + Extensions.swift
+++ MorningDiary/Resource/Extensions/Date + Extensions.swift
@@ -9,6 +9,13 @@ import Foundation
 extension Date {
   static let dateFormatter = DateFormatter()
   
+  static func fullDate(with description: String) -> Date? {
+    Date.dateFormatter.dateStyle = .medium
+    Date.dateFormatter.timeStyle = .none
+    
+    return Date.dateFormatter.date(from: description)
+  }
+  
   func description(with dateFormat: String) -> String {
     Date.dateFormatter.dateFormat = dateFormat
     Date.dateFormatter.locale = .current
