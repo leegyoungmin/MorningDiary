aabc83530423649a84aff524b79904f064564825
미니 <cow970814@naver.com>
2023-05-15 14:12:33 +0900

    feat : Core Data PersistentController 싱글톤 타임 구현

---
+++ MorningDiary/Resource/CoreData/CoreDataManager.swift
@@ -0,0 +1,37 @@
+//
+//  CoreDataManager.swift
+//  MorningDiary
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import CoreData
+
+struct PersistentController {
+  static let shared = PersistentController()
+  
+  let container: NSPersistentContainer
+  
+  init(inMemory: Bool = false) {
+    container = NSPersistentContainer(name: "Diary")
+    container.loadPersistentStores { _, error in
+      if let error = error {
+        fatalError("Unresolved Error \(error)")
+      }
+    }
+  }
+}
+
+private extension PersistentController {
+  func saveContext() {
+    let context = container.viewContext
+    
+    if context.hasChanges {
+      do {
+        try context.save()
+      } catch {
+        let error = error as NSError
+        debugPrint(error.localizedDescription)
+      }
+    }
+  }
+}
