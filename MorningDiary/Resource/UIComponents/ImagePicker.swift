7044ad5fc12ba716172b4202a3b79e177e18a00a
미니 <cow970814@naver.com>
2023-05-16 16:10:24 +0900

    refactor : imagePicker 사진찍는 방법과 사진 앨범에서 고르는 방법 두가지 방법 모두 사용할 수 있도록 변경

--- MorningDiary/Resource/UIComponents/ImagePicker.swift
+++ MorningDiary/Resource/UIComponents/ImagePicker.swift
@@ -7,32 +7,71 @@
 import SwiftUI
 import PhotosUI
 
-struct ImagePicker: UIViewControllerRepresentable {
+protocol Coordinator: AnyObject { }
+
+protocol ImagePickerDelegate: AnyObject {
+  func imagePickerDelegate(picker: Self, didFinishPicking result: UIImage...)
+}
+
+struct ImagePickerView: UIViewControllerRepresentable {
   @Binding var selectedImages: [Photo]
+  let isCamera: Bool
   
-  func makeUIViewController(context: Context) -> some UIViewController {
+  func makeUIViewController(context: Context) -> UIViewController {
+    if isCamera {
+      let controller = configurationCameraViewController()
+      controller.delegate = context.coordinator
+      return controller
+    } else {
+      let controller = configurationImagePickerViewController()
+      controller.delegate = context.coordinator
+      return controller
+    }
+  }
+  
+  private func configurationImagePickerViewController() -> PHPickerViewController {
     var configuration = PHPickerConfiguration()
     configuration.selectionLimit = 5
     configuration.filter = .images
     
     let controller = PHPickerViewController(configuration: configuration)
     controller.isEditing = true
-    controller.delegate = context.coordinator
     
     return controller
   }
   
-  func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) { }
+  private func configurationCameraViewController() -> UIImagePickerController {
+    let controller = UIImagePickerController()
+    controller.sourceType = .camera
+    controller.cameraCaptureMode = .photo
+    controller.cameraDevice = .rear
+    controller.allowsEditing = true
+    
+    return controller
+  }
   
   func makeCoordinator() -> Coordinator {
     return Coordinator(parent: self)
   }
   
-  class Coordinator: NSObject, UINavigationControllerDelegate, PHPickerViewControllerDelegate {
-    let parent: ImagePicker
+  func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) { }
+  
+  class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate, PHPickerViewControllerDelegate {
+    let parent: ImagePickerView
     
-    init(parent: ImagePicker) {
+    init(parent: ImagePickerView) {
       self.parent = parent
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      picker.dismiss(animated: true)
+      guard let editedImage = info[.editedImage] as? UIImage else { return }
+      let photo = Photo(image: editedImage)
+      parent.selectedImages.append(photo)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+      picker.dismiss(animated: true)
     }
     
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
@@ -51,3 +90,5 @@ struct ImagePicker: UIViewControllerRepresentable {
     }
   }
 }
+
+
