3a2d4946aeabbdfe90ab551d9581d4d0f40d10da
미니 <cow970814@naver.com>
2023-05-17 22:54:40 +0900

    refactor : 기획변경에 따른 왼쪽 리스트 뷰 구성 변경

--- MorningDiary/Resource/Scene/DiaryBoardView/DiaryBoardView.swift
+++ MorningDiary/Resource/Scene/DiaryBoardView/DiaryBoardView.swift
@@ -10,24 +10,72 @@ struct DiaryBoardView: View {
   // TODO: - Diary Data 읽어오기
   @State private var selectedContent: DiaryContent?
   
-  var body: some View {
-    GeometryReader { proxy in
-      HStack(spacing: .zero) {
-        NavigationView {
-          DiaryListView(selectedContent: $selectedContent)
-            .navigationTitle("모닝 일기")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-        .navigationViewStyle(.stack)
-        .frame(width: proxy.size.width / 3, height: proxy.size.height)
+  @ViewBuilder
+  func listItemButton(with item: String) -> some View {
+    Button {
+      print("Tapped")
+    } label: {
+      HStack {
+        Label("Example", systemImage: "person.circle")
         
-        NavigationView {
-          DetailDiaryView(selectedContent: $selectedContent)
-            .navigationBarTitleDisplayMode(.inline)
-        }
-        .navigationViewStyle(.stack)
-        .frame(width: proxy.size.width * (2 / 3), height: proxy.size.height)
+        Spacer()
       }
+      .font(.title3)
+      .foregroundColor(.white)
+      .padding(12)
+      .background(Color.red)
+      .cornerRadius(10)
+    }
+    .buttonStyle(.borderless)
+  }
+  
+  var body: some View {
+    NavigationView {
+      List {
+        PlainDisclosureGroup {
+          Text("Example")
+            .font(.system(size: 32, weight: .bold))
+        } content: {
+          ForEach(1..<19) {
+            listItemButton(with: $0.description)
+          }
+        }
+        .listRowSeparator(.hidden)
+      }
+      .listStyle(.plain)
+    }
+  }
+}
+
+struct PlainDisclosureGroup<Label: View, Content: View>: View {
+  @State private var isExpended: Bool = false
+  let label: () -> Label
+  let content: () -> Content
+  
+  init(
+    label: @escaping () -> Label,
+    content: @escaping () -> Content
+  ) {
+    self.label = label
+    self.content = content
+  }
+  
+  var body: some View {
+    HStack {
+      label()
+      Spacer()
+      Image(systemName: "chevron.right")
+        .rotationEffect(.degrees(isExpended ? 90 : .zero))
+    }
+    .contentShape(Rectangle())
+    .onTapGesture {
+      withAnimation {
+        self.isExpended.toggle()
+      }
+    }
+    
+    if isExpended {
+      content()
     }
   }
 }
@@ -35,5 +83,6 @@ struct DiaryBoardView: View {
 struct DiaryBoardView_Previews: PreviewProvider {
   static var previews: some View {
     DiaryBoardView()
+      .previewInterfaceOrientation(.landscapeLeft)
   }
 }
