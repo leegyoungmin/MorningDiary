da2c88b5a8560f00ef53fdaeeb9dda99b229b211
미니 <cow970814@naver.com>
2023-05-18 00:48:33 +0900

    refactor : 디자인 시스템 구현을 위한 타입 이동

--- MorningDiary/Resource/Scene/DiaryBoardView/DiaryBoardView.swift
+++ MorningDiary/Resource/Scene/DiaryBoardView/DiaryBoardView.swift
@@ -17,39 +17,6 @@ struct DiaryBoardView: View {
   }
 }
 
-struct PlainDisclosureGroup<Label: View, Content: View>: View {
-  @State private var isExpended: Bool = true
-  let label: () -> Label
-  let content: () -> Content
-  
-  init(
-    label: @escaping () -> Label,
-    content: @escaping () -> Content
-  ) {
-    self.label = label
-    self.content = content
-  }
-  
-  var body: some View {
-    HStack {
-      label()
-      Spacer()
-      Image(systemName: "chevron.right")
-        .rotationEffect(.degrees(isExpended ? 90 : .zero))
-    }
-    .contentShape(Rectangle())
-    .onTapGesture {
-      withAnimation {
-        self.isExpended.toggle()
-      }
-    }
-    
-    if isExpended {
-      content()
-    }
-  }
-}
-
 struct DiaryBoardView_Previews: PreviewProvider {
   static var previews: some View {
     DiaryBoardView()
